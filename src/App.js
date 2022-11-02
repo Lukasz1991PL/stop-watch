1ce7515fa9a80d71e912fe855b91b2f1ba978c19
Lukasz Bartkowski <lukasz.bartkowski91@gmail.com>
2022-11-02 16:43:17 +0100

    Little Timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [timeOn, setTimeOn] = useState(false);
+  useEffect(() => {
+    let interval = null;
+    if (timeOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [timeOn]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <span>{'0' + Math.floor(time / 1000 / 60 / 60)}:</span>
+        <span>{'0' + (Math.floor(time / 60000) % 60)}:</span>
+        <span>{Math.floor(time / 1000) % 60}:</span>
+        <span>{Math.floor(time / 10) % 100}</span>
+      </div>
+      <div>
+        <button onClick={() => setTimeOn(true)}>Start</button>
+
+        <button onClick={() => setTimeOn(false)}>Stop</button>
+
+        <button onClick={() => setTime(0)}>Reset</button>
+      </div>
     </div>
   );
 }
